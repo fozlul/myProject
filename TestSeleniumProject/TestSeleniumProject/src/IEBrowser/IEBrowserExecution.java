4aeca0429184154d60db6f467e982b664c06dc6a
fozlul <34951586+fozlul@users.noreply.github.com>
2017-12-29 22:00:30 -0500

    2nd Commit

--- TestSeleniumProject/TestSeleniumProject/src/IEBrowser/IEBrowserExecution.java
+++ TestSeleniumProject/TestSeleniumProject/src/IEBrowser/IEBrowserExecution.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class IEBrowserExecution {
 	public static void main(String[] args)
 	{
+		System.out.println("Launcing IE Browser");
+		
 		System.setProperty("webdriver.ie.driver", "C:\\eclipse\\IEDriverServer.exe");
 		WebDriver driver=new InternetExplorerDriver();
 		driver.get("https://www.facebook.com");
 		System.out.println(driver.getTitle());
 		System.out.println("Test End");
+		
 		
 	}
 
